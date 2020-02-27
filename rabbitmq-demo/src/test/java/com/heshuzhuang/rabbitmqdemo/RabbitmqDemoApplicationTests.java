0e9c433207279cc57ac8fd67bd7c9f341db0ba8b
hammer.he <hszandxj@163.com>
2020-02-27 17:45:09 +0800

    码云上传

---
+++ rabbitmq-demo/src/test/java/com/heshuzhuang/rabbitmqdemo/RabbitmqDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heshuzhuang.rabbitmqdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
