5188c4ec8d4c140feb56c61c2c16662c2e519142
hammer.he <hszandxj@163.com>
2020-03-02 16:40:06 +0800

    优化代码

--- rabbitmq-demo/src/main/java/com/heshuzhuang/rabbitmqdemo/config/RabbitmqConfig.java
+++ rabbitmq-demo/src/main/java/com/heshuzhuang/rabbitmqdemo/config/RabbitmqConfig.java
@@ -21,69 +21,6 @@ import org.springframework.context.annotation.Scope;
 @Configuration
 public class RabbitmqConfig {
 	private static final Logger log= LoggerFactory.getLogger(RabbitmqConfig.class);
-//  暂未做
-//	@Autowired
-//	private Environment env;
-//
-//	@Autowired
-//	private CachingConnectionFactory connectionFactory;
-//
-//	@Autowired
-//	private SimpleRabbitListenerContainerFactoryConfigurer factoryConfigurer;
-//
-//	/**
-//	 * 单一消费者
-//	 * @return
-//	 */
-//	@Bean(name = "singleListenerContainer")
-//	public SimpleRabbitListenerContainerFactory listenerContainer(){
-//		SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
-//		factory.setConnectionFactory(connectionFactory);
-//		factory.setMessageConverter(new Jackson2JsonMessageConverter());
-//		factory.setConcurrentConsumers(1);
-//		factory.setMaxConcurrentConsumers(1);
-//		factory.setPrefetchCount(1);
-//		factory.setTxSize(1);
-//		factory.setAcknowledgeMode(AcknowledgeMode.AUTO);
-//		return factory;
-//	}
-//
-//	/**
-//	 * 多个消费者
-//	 * @return
-//	 */
-//	@Bean(name = "multiListenerContainer")
-//	public SimpleRabbitListenerContainerFactory multiListenerContainer(){
-//		SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
-//		factoryConfigurer.configure(factory,connectionFactory);
-//		factory.setMessageConverter(new Jackson2JsonMessageConverter());
-//		factory.setAcknowledgeMode(AcknowledgeMode.NONE);
-////		factory.setConcurrentConsumers(env.getProperty("spring.rabbitmq.listener.concurrency",int.class));
-////		factory.setMaxConcurrentConsumers(env.getProperty("spring.rabbitmq.listener.max-concurrency",int.class));
-////		factory.setPrefetchCount(env.getProperty("spring.rabbitmq.listener.prefetch",int.class));
-//		return factory;
-//	}
-//
-//	@Bean
-//	public RabbitTemplate rabbitTemplate(){
-//		connectionFactory.setPublisherConfirms(true);
-//		connectionFactory.setPublisherReturns(true);
-//		RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-//		rabbitTemplate.setMandatory(true);
-//		rabbitTemplate.setConfirmCallback(new RabbitTemplate.ConfirmCallback() {
-//			@Override
-//			public void confirm(CorrelationData correlationData, boolean ack, String cause) {
-//				log.info("消息发送成功:correlationData({}),ack({}),cause({})",correlationData,ack,cause);
-//			}
-//		});
-//		rabbitTemplate.setReturnCallback(new RabbitTemplate.ReturnCallback() {
-//			@Override
-//			public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
-//				log.info("消息丢失:exchange({}),route({}),replyCode({}),replyText({}),message:{}",exchange,routingKey,replyCode,replyText,message);
-//			}
-//		});
-//		return rabbitTemplate;
-//	}
 
 	/**
 	 * 第二种 配置rabbitmq方案
