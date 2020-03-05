529fedd31d3514b4caab1d690959271c3efcc790
hammer.he <hszandxj@163.com>
2020-03-05 15:03:53 +0800

    何书壮   测试实现不同的实现类

---
+++ springboot-dubbo-nacos-provider/src/main/java/com/example/heshuzhuang/springbootdubbonacosprovider/impl/CeshiImpl1.java
@@ -0,0 +1,19 @@
+package com.example.heshuzhuang.springbootdubbonacosprovider.impl;
+
+import com.example.hehsuzhuang.springbootdubbonacosbase.service.ICeshi;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.config.annotation.Service;
+
+/**
+ * @author heshuzhuang
+ * @Date 2020/3/5 13:57
+ */
+@Slf4j
+@Service(interfaceName = "ceshi1")
+public class CeshiImpl1 implements ICeshi {
+	@Override
+	public String ceshi() {
+	log.info("走到了测试1");
+		return "测试1";
+	}
+}
---
+++ springboot-dubbo-nacos-provider/src/main/java/com/example/heshuzhuang/springbootdubbonacosprovider/impl/CeshiImpl2.java
@@ -0,0 +1,20 @@
+package com.example.heshuzhuang.springbootdubbonacosprovider.impl;
+
+import com.example.hehsuzhuang.springbootdubbonacosbase.service.ICeshi;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.config.annotation.Service;
+
+/**
+ * @author heshuzhuang
+ * @Date 2020/3/5 13:57
+ */
+@Slf4j
+@Service(interfaceName = "ceshi2")
+public class CeshiImpl2 implements ICeshi {
+
+	@Override
+	public String ceshi() {
+		log.info("走到了测试2");
+		return "测试2";
+	}
+}
