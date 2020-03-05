10afbf686a58a3826a7f82d1dc15ad0beba7c274
hammer.he <hszandxj@163.com>
2020-03-05 10:26:45 +0800

    何书壮   nacosdemo

---
+++ springboot-dubbo-nacos-base/src/main/java/com/example/hehsuzhuang/springbootdubbonacosbase/SpringbootDubboNacosBaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.hehsuzhuang.springbootdubbonacosbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDubboNacosBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDubboNacosBaseApplication.class, args);
+	}
+
+}
---
+++ springboot-dubbo-nacos-base/src/test/java/com/example/hehsuzhuang/springbootdubbonacosbase/SpringbootDubboNacosBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hehsuzhuang.springbootdubbonacosbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDubboNacosBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
