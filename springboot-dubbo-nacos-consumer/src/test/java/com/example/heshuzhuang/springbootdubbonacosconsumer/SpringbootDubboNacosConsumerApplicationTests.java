7cef7899d28a312a7ff6da7e9d5ac8fc0e6a0abd
hammer.he <hszandxj@163.com>
2020-03-05 10:44:04 +0800

    何书壮   nacosdemo

---
+++ springboot-dubbo-nacos-consumer/src/test/java/com/example/heshuzhuang/springbootdubbonacosconsumer/SpringbootDubboNacosConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.heshuzhuang.springbootdubbonacosconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDubboNacosConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
