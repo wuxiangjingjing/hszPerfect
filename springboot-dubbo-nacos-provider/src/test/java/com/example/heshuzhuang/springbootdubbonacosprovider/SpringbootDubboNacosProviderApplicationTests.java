62ed31f1f46436288762d29f095e1bcf59d281fe
hammer.he <hszandxj@163.com>
2020-03-05 10:24:42 +0800

    何书壮   nacosdemo

---
+++ springboot-dubbo-nacos-provider/src/test/java/com/example/heshuzhuang/springbootdubbonacosprovider/SpringbootDubboNacosProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.heshuzhuang.springbootdubbonacosprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDubboNacosProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
