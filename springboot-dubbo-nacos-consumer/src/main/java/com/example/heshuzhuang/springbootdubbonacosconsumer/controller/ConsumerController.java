059ff37ec10b17d40cdfad84081613af1c94062d
hammer.he <hszandxj@163.com>
2020-03-05 15:02:17 +0800

    何书壮   测试dubbo 调用不同实现类型

--- springboot-dubbo-nacos-consumer/src/main/java/com/example/heshuzhuang/springbootdubbonacosconsumer/controller/ConsumerController.java
+++ springboot-dubbo-nacos-consumer/src/main/java/com/example/heshuzhuang/springbootdubbonacosconsumer/controller/ConsumerController.java
@@ -2,7 +2,9 @@ package com.example.heshuzhuang.springbootdubbonacosconsumer.controller;
 
 import com.example.hehsuzhuang.springbootdubbonacosbase.dto.ProviderTestDto;
 import com.example.hehsuzhuang.springbootdubbonacosbase.dto.ResultVO;
+import com.example.hehsuzhuang.springbootdubbonacosbase.service.ICeshi;
 import com.example.hehsuzhuang.springbootdubbonacosbase.service.IProviderService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,16 +18,39 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/consumer")
+@Slf4j
 public class ConsumerController {
 
 
 	@Reference
 	private IProviderService providerService;
+
+	@Reference
+	private ICeshi ceshi1;
+//	@Reference
+//	@Qualifier("ceshi2")
+//	private ICeshi ceshi12;
+
+
+
+
 	@RequestMapping(value = "/list",method = RequestMethod.GET)
 	public ResultVO getList(){
 		// 远程调用
 		List<ProviderTestDto> providerTestDTOList = providerService.queryList();
 		return new ResultVO.Builder<>().code(200).message("success").data(providerTestDTOList).build();
+	}
+
+	/**
+	 * 不同情况的调用
+	 * @param i
+	 * @return
+	 */
+	@RequestMapping(value = "/ceshi",method = RequestMethod.GET)
+	public String getResult(Integer i) {
+		log.info("获取的i值是" +  i);
+		// 远程调用
+		return ceshi1.ceshi();
 	}
 
 
