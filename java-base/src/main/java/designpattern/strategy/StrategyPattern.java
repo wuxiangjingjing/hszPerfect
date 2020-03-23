188cd2103e43a30f0da7fe526e62d1ce7a7853a8
hammer.he <hszandxj@163.com>
2020-03-23 14:00:26 +0800

    何书壮   策略模式  小demo 上传

---
+++ java-base/src/main/java/designpattern/strategy/StrategyPattern.java
@@ -0,0 +1,65 @@
+package designpattern.strategy;
+
+/**
+ *
+ * 策略模式   小demo
+ * @author heshuzhuang
+ * @Date 2020/3/23 13:52
+ */
+public class StrategyPattern {
+
+	/**
+	 * 测试不同的实现，在实际应用中多是控制层调用
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Config c = new Config( new A());
+		c.hahah();
+
+	}
+
+}
+
+/**
+ * 定义一个接口
+ */
+interface Heshuzhuang{
+	void sayHelloWorld();
+}
+
+/**
+ * 不同的实现类    来实现不同的策略
+ */
+class A implements  Heshuzhuang{
+
+	@Override
+	public void sayHelloWorld() {
+		System.out.print("执行了A方法");
+	}
+}
+
+class B implements  Heshuzhuang{
+
+	@Override
+	public void sayHelloWorld() {
+		System.out.print("执行了B方法");
+	}
+}
+// 环境配置类
+
+class Config {
+	private Heshuzhuang heshuzhuang;
+
+	public  Config(Heshuzhuang heshuzhuang){
+		this.heshuzhuang = heshuzhuang;
+	}
+
+	public void hahah(){
+		heshuzhuang.sayHelloWorld();
+	}
+
+}
+
+
+
+
