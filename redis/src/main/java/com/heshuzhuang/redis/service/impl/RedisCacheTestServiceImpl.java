d8413957849f96dd62a861465938db3fefaf389f
hammer.he <hszandxj@163.com>
2020-03-24 17:54:45 +0800

    何书壮   缓存配置的更改

--- redis/src/main/java/com/heshuzhuang/redis/service/impl/RedisCacheTestServiceImpl.java
+++ redis/src/main/java/com/heshuzhuang/redis/service/impl/RedisCacheTestServiceImpl.java
@@ -1,6 +1,7 @@
 package com.heshuzhuang.redis.service.impl;
 
 import com.heshuzhuang.redis.service.RedisCacheTestService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -9,11 +10,18 @@ import org.springframework.stereotype.Service;
  * @Date 2020/3/24 14:49
  */
 @Service
+@Slf4j
 public class RedisCacheTestServiceImpl implements RedisCacheTestService {
 
+
+	/**
+	 * value 和 cacheNames 不可以同时设置
+	 * @return
+	 */
 	@Override
-	@Cacheable(cacheNames = "hsz" ,value = "111111111")
+	@Cacheable(cacheNames = "hsz" ,key = "111111111")
 	public String testRedis() {
+		log.info("进来了否");
 		return "我是你的巴巴变";
 	}
 }
